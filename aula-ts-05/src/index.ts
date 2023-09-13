d9adf41516a98bc07f4bb04a0c4b2ed80f894808
vitorluizms <vitor.luiz.eer@gmail.com>
2023-09-13 16:12:43 -0300

    exercicio 5

---
+++ aula-ts-05/src/index.ts
@@ -0,0 +1,5 @@
+const numbers: Array<number> = [1, 2, 3, 4, 5];
+const anwsers: Array<boolean> = [true, false, false, true, false];
+const strings: Array<string> = ["one", "two", "three", "four", "five"];
+const mix: Array<number | string> = [1, "two", 3, "four", 5];
+const floatNumbers: Array<number> = [1.0, 2.5, 3.0, 4.1, 5.5];
